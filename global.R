5575f21f1ebd397946bad42468d2a73f5d7c7ba7
kdaily <kdaily@users.noreply.github.com>
2015-12-01 14:51:47 -0800

    change for PEC specific.

--- global.R
+++ global.R
@@ -5,17 +5,15 @@ library(stringr)
 library(synapseClient)
 
 synapseLogin()
-benefactorId <- "syn1773109"
+projectId <- "syn2787333"
 
 # Which columns to consider
-colsToUse <- c('id', 'dataType', 'fileType', 'fileSubType', 'UID', 'biologicalSampleName', 
-               'C4_Cell_Line_ID', 'Originating_Lab_ID', 'Originating_Lab', 'Cell_Line_Type',
-               'Cell_Type_of_Origin', 'Tissue_of_Origin', 'Reprogramming_Vector_Type',
-               'Reprogramming_Gene_Combination', 'pass_qc', 'exclude')
+colsToUse <- c('id', 'assay', 'fileType', 'assayTarget', 'organism', 'disease',
+               'tissueType', 'cellType', 'study', 'group', 'name')
 
 # query the table with specific columns
 colsToUseStr <- paste(colsToUse, collapse=",")
-queryTemplate <- "select %s from file where benefactorId=='%s'"
+queryTemplate <- "select %s from file where projectId=='%s'"
   
 allData <- synQuery(sprintf(queryTemplate, colsToUseStr, benefactorId), 
                     blockSize=300)$collectAll()
@@ -23,7 +21,10 @@ colnames(allData) <- gsub(".*\\.", "", colnames(allData))
 allData <- allData[, colsToUse]
 
 # Filter
-allData <- allData %>% filter(dataType != "", !is.na(dataType))
+allData <- allData %>% filter(!is.na(fileType))
+
+# Convert to factors for selectize filtering in UI
+allData <- allData %>% mutate_each(funs(factor), -id, -name)
 
 # Turn IDs into urls that open in new tab/window
 allData$id <- paste('<a href="https://www.synapse.org/#!Synapse:', allData$id, 
