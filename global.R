d0a18c30630d1b818bffd0b3ff56a181a5445766
thomasyu888 <thomasyu888@gmail.com>
2015-12-21 00:46:02 -0800

    Load R object instead of doing query everytime

--- global.R
+++ global.R
@@ -5,31 +5,33 @@ library(stringr)
 library(synapseClient)
 
 synapseLogin()
-projectId <- "syn2942337"
+
+#projectId <- "syn2942337"
 
 # Which columns to consider
-colsToUse <- c('id','labId','index','diagnosis','diagnosisClass',
-               'sampleGroup','algorithm','coreFlowCellId',
-               'flowCell','fileSize','modifiedDate','fileType',
-               'coreSampleId','lane')
+#colsToUse <- c('id','labId','diagnosis','diagnosisClass',
+#               'sampleGroup','algorithm',
+#               'flowCell','fileSize','modifiedDate','fileType',
+ #              'coreSampleId')
 
 # query the table with specific columns
-colsToUseStr <- paste(colsToUse, collapse=",")
-queryTemplate <- "select %s from file where projectId=='%s'"
+#colsToUseStr <- paste(colsToUse, collapse=",")
+#queryTemplate <- "select %s from file where benefactorId=='%s'"
   
-allData <- synQuery(sprintf(queryTemplate, colsToUseStr, projectId), 
-                    blockSize=300)$collectAll()
+#allData <- synQuery(sprintf(queryTemplate, colsToUseStr, projectId), 
+#                    blockSize=300)$collectAll()
 
-colnames(allData) <- gsub(".*\\.", "", colnames(allData))
-allData <- allData[, colsToUse]
+#colnames(allData) <- gsub(".*\\.", "", colnames(allData))
+#allData <- allData[, colsToUse]
 
 # Filter
-allData <- allData %>% filter(labId != "", !is.na(labId))
+#allData <- allData %>% filter(labId != "", !is.na(labId))
 
 # Turn IDs into urls that open in new tab/window
-allData$id <- paste('<a href="https://www.synapse.org/#!Synapse:', allData$id, 
-                    '" target="_blank">', allData$id, '</a>', sep="")
-
+#allData$id <- paste('<a href="https://www.synapse.org/#!Synapse:', allData$id, 
+#                    '" target="_blank">', allData$id, '</a>', sep="")
+queryResults <- synGet("syn5561877")
+load(queryResults@filePath)
 # # Filtering for specific data types and making the data for specific tabs
 # # Just an example how to use tidyr and dplyr to reformat.
 # mrna <- allData %>% 
